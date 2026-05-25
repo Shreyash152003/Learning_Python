9ff1890f58d63e29730f29da35e2aeba6463084f
Shreyash <shreyashjanbandhu7@gmail.com>
2026-05-25 15:32:47 +0530

    The python journey has began

---
+++ Basic.py
@@ -0,0 +1,29 @@
+print("Hello")
+print("Hello Shreyash")
+#This is the beginning of  python programming
+'''
+I am the one with white blood cells
+'''
+#The code of python is executed from Top to Bottom
+
+print("My name is Shreyash.")
+print("My College name is Government College Of Engineering Aurangabad, Chh. Sambhajinagar")
+print("My Dream compnay is the company that select me as a fresher because it is the one that shows trust on me")
+
+#Printting Pattern
+for i in range (3):
+    for j in range (3):
+       print("* ",end="")
+    print()
+
+
+a = int(input("Enter your age : "))
+print("Your age is ",a)
+
+name = input("Enter your name : ")
+college = input("Enter your college Name : ")
+goal = input("What is your goal : ")
+
+print("Your name is ",name)
+print("Your College is ",college)
+print("Your goal is ",goal)
